77517be905fa96caeb1aa392efb82a6e8b267111
Sridhar Bandi <sridhar.bandi.ece@gmail.com>
2024-10-09 20:01:53 +0100

    Mobile Status Bar Color

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { ThemeProvider } from '../components/ThemeProvider';
 import '../styles/globals.css';
 import '../styles/burger-menu-animation.css';
@@ -6,6 +7,10 @@ import '../styles/burger-menu-animation.css';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
+      <Head>
+        <meta name="theme-color" content="#8B5CF6" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+      </Head>
       <Component {...pageProps} />
     </ThemeProvider>
   );
